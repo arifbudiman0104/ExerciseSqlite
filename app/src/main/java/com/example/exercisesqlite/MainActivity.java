8497f1b2bb632ad5aab33658cec8fc3082a48a2a
Arif Budiman Arrosyid <arif.budiman.ft18@mail.umy.ac.id>
2020-04-15 15:51:15 +0700

    Initial commit

--- app/src/main/java/com/example/exercisesqlite/MainActivity.java
+++ app/src/main/java/com/example/exercisesqlite/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.exercisesqlite;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fab =findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent i = new Intent(getApplicationContext(), tambah_data.class);
+                startActivity(i);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/exercisesqlite/tambah_data.java
@@ -0,0 +1,13 @@
+package com.example.exercisesqlite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class tambah_data extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tambah_data);
+    }
+}
